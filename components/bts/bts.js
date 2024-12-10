4648117edef6acb3a471b6c3cc2de2dd0019c1f8
ABDULMATEEN SHAMSI <abdulmateen@192.168.2.109>
2024-12-10 20:07:23 +0530

    Modified page

--- components/bts/bts.js
+++ components/bts/bts.js
@@ -1,9 +1,14 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const cards = document.querySelectorAll('.card');
+const panels = document.querySelectorAll('.panel')
 
-    cards.forEach(card => {
-      card.addEventListener('click', () => {
-        card.classList.toggle('expanded');
-      });
-    });
-  });
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
