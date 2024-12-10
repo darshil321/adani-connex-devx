597057a9c4b83e8dd6b5a2776db90ed72d794f2c
ABDULMATEEN SHAMSI <abdulmateen@ABDULMATEENs-MacBook-Air.local>
2024-12-10 15:20:36 +0530

    MOdified page

--- components/bts/bts.js
+++ components/bts/bts.js
@@ -1,20 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const cards = document.querySelectorAll('.card');
-    let expandedCard = null;
 
     cards.forEach(card => {
-        card.addEventListener('click', function() {
-            if (expandedCard && expandedCard !== this) {
-                expandedCard.classList.remove('card-expanded');
-            }
-            
-            if (this === expandedCard) {
-                this.classList.remove('card-expanded');
-                expandedCard = null;
-            } else {
-                this.classList.add('card-expanded');
-                expandedCard = this;
-            }
-        });
+      card.addEventListener('click', () => {
+        card.classList.toggle('expanded');
+      });
     });
-});
+  });
